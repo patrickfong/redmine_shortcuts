37baca49db1436944639cd5e207ce236cd7ab310
Patrick Fong <patrickspfong@gmail.com>
2023-09-02 22:26:51 -0700

    Add navigation shortcut for My page and Projects

--- assets/javascripts/view_layouts_base_html_head.js
+++ assets/javascripts/view_layouts_base_html_head.js
@@ -9,6 +9,14 @@ $(document).keydown(function (e) {
         if (!e.ctrlKey && !e.altKey && !e.shiftKey && e.keyCode == 83) {
             $("#q").focus();
             e.preventDefault();
+        // M
+        } else if (e.keyCode == 77) {
+            window.location.href = '/my/page';
+            e.preventDefault();
+        // P
+        } else if (e.keyCode == 80) {
+            window.location.href = '/projects';
+            e.preventDefault();
         }
     }
 });
