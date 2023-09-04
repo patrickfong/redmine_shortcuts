e28ff4fd510c620efe2c7c04c72d3e3dc3750d73
Patrick Fong <patrickspfong@gmail.com>
2023-09-04 15:55:18 -0700

    Reorganize shortcut keys and revise help text.

--- assets/javascripts/view_issues_index_bottom.js
+++ assets/javascripts/view_issues_index_bottom.js
@@ -1,11 +1,7 @@
 $(document).keydown(function (e) {
     if (!$(':focus').is('input') && !$(':focus').is('select') && !$(':focus').is('textarea')) {
         if (!e.ctrlKey && !e.altKey && !e.shiftKey && !e.metaKey) {
-            // N
-            if (e.keyCode == 78) {
-                $(location).attr('href', $('.icon-add').attr('href'));
-                e.preventDefault();
-            }
+            // New issue shortcut key implementation removed
         }
     }
 });
